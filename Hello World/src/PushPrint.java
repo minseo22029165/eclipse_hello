be440b17376206682e213079942d792b817b0e1f
minseo22029165 <102271676+minseo22029165@users.noreply.github.com>
2022-04-07 11:16:21 +0900

    Update PushPrint.java
    PushPrint.java modified on GitHub

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -2,6 +2,6 @@
 public class PushPrint {
   public static void main(String[] args) {
   System.out.println("Add java file");
-    System.out.println("Hello in local");
+    System.out.println("Hello...");
   }
 }
